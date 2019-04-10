c97666c1d9f7fdb48446f28e55ec29d3f27a3322
Greaux <22226093+Greaux@users.noreply.github.com>
2019-04-10 12:59:06 +0300

    Update Program.cs
    Rewrited code

--- Program.cs
+++ Program.cs
@@ -1,48 +1,61 @@
-﻿using System;
-using System.Threading;
-using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace Owls_Network_locker
+using System.Diagnostics;
+
+namespace Greaux
 {
     class Program
-    {      
-        static void Main(string[] args)
-        {
-            string networkInterfaceName = "Беспроводная сеть 2";
-            while (true)
-            {
-                Process[] pname = Process.GetProcessesByName("tallow");
-                if(pname.Length == 0)
-                {
-                    Console.WriteLine("Problem");
-                    Task TaskOne = Task.Factory.StartNew(() => DisableAdapter(networkInterfaceName) );
-                    // - отключение адаптера
-                    Console.ReadLine();
-                    // - запуск
-                    Task TaskTwo = Task.Factory.StartNew(() => EnableAdapter(networkInterfaceName) );
-                } 
-                else
-                {
-                    Console.WriteLine("All is OK");
-                   Thread.Sleep(1000);
-                }
-            }
-        }
-                static void EnableAdapter(string interfaceName)
+    {
+        static void EnableAdapter(string interfaceName)
         {
             ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" enable");
             Process p = new Process();
             p.StartInfo = psi;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
             p.Start();
         }
-
         static void DisableAdapter(string interfaceName)
         {
             ProcessStartInfo psi = new ProcessStartInfo("netsh", "interface set interface \"" + interfaceName + "\" disable");
             Process p = new Process();
             p.StartInfo = psi;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
             p.Start();
+        }
+        static bool isStarted(string name)
+        {
+            Process[] allProcess = Process.GetProcesses();
+            foreach(Process p in allProcess)
+            {
+                if (p.ProcessName == name)
+                    return true;
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {              
+                if (!isStarted("tallow"))
+                {
+                    Console.WriteLine("Problem");
+                    DisableAdapter("Ethernet0");
+                    Console.ReadKey();
+                    EnableAdapter("Ethernet0");
+                }
+                else
+                {
+                    Console.WriteLine("All is OK");
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
         }
     }
 }
